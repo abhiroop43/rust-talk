51a77aa306ecc23c8c6789d6e5a0ffcd0677ac16
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-11-29 08:50:25 +0530

    feat(api): Add conversation endpoint and inference callback

--- src/api.rs
+++ src/api.rs
@@ -1,18 +1,20 @@
+use std::convert::Infallible;
+
 use crate::model::conversation::Conversation;
+use cfg_if::cfg_if;
 use leptos::*;
 
-#[server(Converse "/api")]
-pub async fn converse(cs: Scope, prompt: Conversation) -> Result<String, ServerFnError> {
+#[server(Converse, "/api")]
+pub async fn converse(prompt: Conversation) -> Result<String, ServerFnError> {
     use actix_web::dev::ConnectionInfo;
     use actix_web::web::Data;
     use leptos_actix::extract;
     use llm::models::Llama;
 
-    let model = extract(cx, |data: Data<Llama>, _connection: ConnectionInfo| async {
-        data.into_inner();
-    })
-    .await
-    .unwrap();
+    let model =
+        extract(|data: Data<Llama>, _connection: ConnectionInfo| async { data.into_inner() })
+            .await
+            .unwrap();
 
     use llm::KnownMoodel;
 
@@ -27,7 +29,7 @@ pub async fn converse(cs: Scope, prompt: Conversation) -> Result<String, ServerF
 
     for message in prompt.messages.into_iter() {
         let msg = message.text;
-        let curr_line = if message.user {
+        let curr_line = if message.is_user {
             format!("{character_name}: {msg}\n")
         } else {
             format!("{user_name}: {msg}\n")
@@ -36,5 +38,68 @@ pub async fn converse(cs: Scope, prompt: Conversation) -> Result<String, ServerF
         history.push_str(&curr_line);
     }
 
-    Ok(String::from(""))
+    let mut res = String::new();
+    let mut rng = rand::thread_rng();
+    let mut buf = String::new();
+
+    let mut session = model.start_session(Default::default());
+
+    session
+        .infer(
+            model.as_ref(),
+            &mut rng,
+            &llm::InferenceRequest {
+                prompt: format!("{persona}\n{history}\n{character_name}:")
+                    .as_str()
+                    .into(),
+                parameters: &llm::InferenceParameters::default(),
+                play_back_previous_tokens: false,
+                maximum_token_count: None,
+            },
+            &mut Default::default(),
+            inference_callback(String::from(user_name), &mut buf, &mut res),
+        )
+        .unwrap_or_else(|e| panic!("{e}"));
+
+    Ok(res)
+}
+
+cfg_if! {
+    if #[cfg(feature = "ssr")] {
+        use std::convert::Infallible;
+            fn inference_callback<'a>(
+                stop_sequence: String,
+                buf: &'a mut String,
+                out_str: &'a mut String,
+            ) -> impl FnMut(llm::InferenceRequest) -> Result<llm::InferenceFeedback, Infallible> + 'a {
+                use llm::InferenceFeedback::Halt;
+                use llm::InferenceFeedback:;Continue;
+
+                move |resp| match resp {
+                    llm::InferenceResponse::InferredToken(t) => {
+                        let mut reverse_buf = buf.clone();
+                        reverse_buf.push_str(t.as_str());
+
+                        if stop_sequence.as_str().eq(reverse_buf.as_str()) {
+                            buf.clear();
+                            return Ok::<llm::InferenceFeedback, Infallible>(Halt);
+                        } else if stop_sequence.as_str().starts_with(reverse_buf.as_str()) {
+                            buf.push_str(t.as_str());
+                            return Ok(Continue);
+                        }
+
+                        if buf.is_empty() {
+                            out_str.push_str(&t);
+                        } else {
+                            out_str.push_str(&reverse_buf);
+                        }
+
+                        Ok(Continue)
+                    }
+
+                    llm::InferenceResponse::EotToken => Ok(Halt),
+                    _ => Ok(Continue),
+                }
+            }
+    }
 }
