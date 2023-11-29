75a94bc291686dc70f26eb4480cf46a196d76869
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-11-29 12:58:49 +0530

    style: add styling for chat box section and history section

--- src/app/components/chat_box_section.rs
+++ src/app/components/chat_box_section.rs
@@ -4,18 +4,17 @@ use leptos::{html::Input, *};
 pub fn ChatBoxSection(send: Action<String, Result<String, ServerFnError>>) -> impl IntoView {
     let input_ref = create_node_ref::<Input>();
     view! {
-      <div>
-        <form on:submit=move |ev| {
-          ev.prevent_default();
-          let input = input_ref.get().expect("input to exist");
-
-          send.dispatch(input.value());
-          input.set_value("");
-        }>
-          <input type="text" class="w-2/3 p-4 border border-gray-300 rounded-full" node_ref=input_ref />
-          <input type="submit" value="Send" class="h-full p-4 bg-blue-500 text-white rounded-full cursor-pointer" />
-        </form>
-      </div>
-
+        <div class="h-24 w-full fixed bottom-0 flex justify-center items-center p-5 bg-white border-t border-gray-300">
+           <form class="w-full flex justify-center items-center gap-4" on:submit=move |ev| {
+                ev.prevent_default();
+                let input = input_ref.get().expect("input to exist");
+                send.dispatch(input.value());
+                input.set_value("");
+           }
+           >
+                <input class="w-2/3 p-4 border border-gray-300 rounded-full" type="text" placeholder="Enter your prompt" node_ref=input_ref/>
+                <input class="h-full p-4 bg-blue-500 text-white rounded-full cursor-pointer" type="submit" value="Send" />
+           </form>
+        </div>
     }
 }
